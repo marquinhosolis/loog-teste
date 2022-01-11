85f491addd4fdd469a18bb7649f6e8a0260b8fa7
Marco Solis <marquinhosolis@gmail.com>
2022-01-11 20:00:15 -0300

    Adicionando icones ao menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './Menu.scss';
 
+import { IoLogoAngular } from 'react-icons/io5';
+import { IoLogoVue } from 'react-icons/io5';
+import { IoLogoReact } from 'react-icons/io5';
+
 export default function Menu() {
 	return (
 		<nav>
 			<ul>
 				<li>
-					<a href="#">Angular</a>
+					<a href="#">
+						<IoLogoAngular />
+						<span>Angular</span>
+					</a>
 				</li>
 				<li>
-					<a href="#">Facebook React</a>
+					<a href="#">
+						<IoLogoReact />
+						<span>React</span>
+					</a>
 				</li>
 				<li>
-					<a href="#">VueJs</a>
+					<a href="#">
+						<IoLogoVue />
+						<span>VueJs</span>
+					</a>
 				</li>
 			</ul>
 		</nav>
