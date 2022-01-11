840e1df575791bd5795c9668515f0e09479fecf3
Marco Solis <marquinhosolis@gmail.com>
2022-01-11 19:43:05 -0300

    comportamento basico do menu

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Menu.scss';
+
+export default function Menu() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Angular</a>
+				</li>
+				<li>
+					<a href="#">Facebook React</a>
+				</li>
+				<li>
+					<a href="#">VueJs</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.scss';
 import './index.scss';
+import Menu from './components/Menu/Menu';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<h1>Teste prático Loog</h1>
+		<div className="pageWrapper">
+			{/* menu */}
+			<Menu />
+			{/* area principal da pagina */}
+			<main></main>
+		</div>
+
+		{/* titulo da pagina */}
+		{/* searchbox */}
+		{/* usuários do grupo */}
+		{/* detalhe do usuário */}
 	</React.StrictMode>,
 	document.getElementById('root')
 );
