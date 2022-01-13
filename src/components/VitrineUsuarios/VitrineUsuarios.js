5fe9f3874001d4a2b8c2661cf66d1bc51eceb2c7
Marco Solis <marquinhosolis@gmail.com>
2022-01-13 00:33:45 -0300

    Conectando a api do github e trazendo os resultados

--- src/components/VitrineUsuarios/VitrineUsuarios.js
+++ src/components/VitrineUsuarios/VitrineUsuarios.js
@@ -1,137 +1,35 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import './VitrineUsuarios.scss';
 
 export default function VitrineUsuarios(props) {
+	const [membros, setMembros] = useState([]);
+	const endPoint = `http://api.github.com/orgs/${props.grupo}/members`;
+	const urlApi = `${endPoint}?per_page=10&page=1`;
+
+	// useEffect para controlar a mudanca do grupo
+	useEffect(() => {
+		// conecta à api do github e seta os membros com a resposta
+		fetch(urlApi)
+			.then((response) => response.json())
+			.then((data) => setMembros(data))
+			.catch((error) => console.log(error));
+	}, [props.grupo]);
+
 	return (
 		<div className="usuariosWrapper">
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
+			{membros.map((membro) => (
+				<a href="#">
+					<div className="usuarioCard">
+						<div className="usuarioAvatar">
+							<img
+								src={`https://github.com/${membro.login}.png`}
+								alt={membro.login}
+							/>
+						</div>
+						<div className="usuarioNome">{membro.login}</div>
 					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
-			<a href="#">
-				<div className="usuarioCard">
-					<div className="usuarioAvatar">
-						<img src="https://picsum.photos/200/300" alt="" />
-					</div>
-					<div className="usuarioNome">Nome do Usuário</div>
-				</div>
-			</a>
+				</a>
+			))}
 		</div>
 	);
 }
