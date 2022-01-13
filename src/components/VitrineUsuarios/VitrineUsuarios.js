5e805ea8f2b5cc1eccad530629e29d75b3897fb2
Marco Solis <marquinhosolis@gmail.com>
2022-01-13 01:18:15 -0300

    Adicionando rolagem infitina na vitrine de membros

--- src/components/VitrineUsuarios/VitrineUsuarios.js
+++ src/components/VitrineUsuarios/VitrineUsuarios.js
@@ -3,11 +3,17 @@ import './VitrineUsuarios.scss';
 
 export default function VitrineUsuarios(props) {
 	const [membros, setMembros] = useState([]);
+	const [paginaAtual, setPaginaAtual] = useState(1);
+
+	// monta a url da api
 	const endPoint = `http://api.github.com/orgs/${props.grupo}/members`;
-	const urlApi = `${endPoint}?per_page=10&page=1`;
+	const urlApi = `${endPoint}?per_page=15&page=${paginaAtual}`;
 
 	// useEffect para controlar a mudanca do grupo
 	useEffect(() => {
+		//reseta os membros e a paginacao quando o grupo mudar
+		setMembros([]);
+		setPaginaAtual(1);
 		// conecta à api do github e seta os membros com a resposta
 		fetch(urlApi)
 			.then((response) => response.json())
@@ -15,10 +21,30 @@ export default function VitrineUsuarios(props) {
 			.catch((error) => console.log(error));
 	}, [props.grupo]);
 
+	// useEffect para controlar a mudanca da pagina
+	useEffect(() => {
+		fetch(urlApi)
+			.then((response) => response.json())
+			.then((novosMembros) =>
+				setMembros((prevMembros) => [...prevMembros, ...novosMembros])
+			)
+			.catch((error) => console.log(error));
+	}, [paginaAtual]);
+
+	useEffect(() => {
+		const intersectionObserver = new IntersectionObserver((entries) => {
+			if (entries.some((entry) => entry.isIntersecting)) {
+				setPaginaAtual((paginaAtualState) => paginaAtualState + 1);
+			}
+		});
+		intersectionObserver.observe(document.querySelector('#finalRolagem'));
+		return () => intersectionObserver.disconnect();
+	}, []);
+
 	return (
 		<div className="usuariosWrapper">
 			{membros.map((membro) => (
-				<a href="#">
+				<a href="#" key={membro.id}>
 					<div className="usuarioCard">
 						<div className="usuarioAvatar">
 							<img
@@ -30,6 +56,7 @@ export default function VitrineUsuarios(props) {
 					</div>
 				</a>
 			))}
+			<div id="finalRolagem"></div>
 		</div>
 	);
 }
