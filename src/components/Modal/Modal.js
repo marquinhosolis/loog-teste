6d6e8b87d1e66404d91483872e2ac082719462f9
Marco Solis <marquinhosolis@gmail.com>
2022-01-14 01:54:30 -0300

    bugFix: importando useEffect no componente Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import './Modal.scss';
 import { IoClose } from 'react-icons/io5';
 import UserAvatar from '../UserAvatar/UserAvatar';
